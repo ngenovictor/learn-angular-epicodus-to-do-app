1872b716c3c688437e3c1a5ebeacbad50546afb1
Victor Ngeno <victor.ngeno@gro-intelligence.com>
2019-02-21 19:59:50 +0300

    can list to do items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'to-do';
+  title = 'To-do';
+  task = new Task('Do dishes', 'high');
+  tasks: Task[] = [
+    new Task('Do dishes', 'high'),
+    new Task('Do homework', 'high'),
+    new Task('Beat Tony', 'high'),
+    new Task('Learn To code', 'high'),
+    new Task('Lauch a website', 'high'),
+    new Task('Lose weight', 'high'),
+  ]
 }
