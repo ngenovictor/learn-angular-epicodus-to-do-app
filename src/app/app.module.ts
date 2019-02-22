7900ef6551e27d7b401873cf08c140d64b85072f
Victor Ngeno <victor.ngeno@gro-intelligence.com>
2019-02-22 20:00:34 +0300

    add components to edit and add new tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { TaskListComponent } from './task-list/task-list.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
+import { AddTaskComponent } from './add-task/add-task.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent
+    TaskListComponent,
+    TaskEditComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
