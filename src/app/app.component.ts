8a7f052840c27d276c7991eff5336a0699cfcdd8
Victor Ngeno <victor.ngeno@gro-intelligence.com>
2019-02-22 08:44:29 +0300

    can now list our tasks on new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,33 +9,9 @@ import { Task } from './task.model';
 
 export class AppComponent {
   title = 'To-do';
-  tasks: Task[] = [
-    new Task('Do dishes', 'high'),
-    new Task('Do homework', 'high'),
-    new Task('Beat Tony', 'high'),
-    new Task('Learn To code', 'high'),
-    new Task('Lauch a website', 'high'),
-    new Task('Lose weight', 'high'),
-  ]
-  colourTasks(task: Task): string{
-    if(task.done){
-      return "bg-info"
-    }else{
-      return "bg-danger"
-    }
-  }
+  
   selectedTask: Task;
-  editTask(task?: Task){
-    if(task){
-      this.selectedTask = task;
-    }else{
-      this.selectedTask = null;
-    }
-  }
-  deleteTask(task: Task){
-    let task_index = this.tasks.indexOf(task);
-    this.tasks.splice(task_index, 1);
-  }
+  
   addNewTask(){
     let createdTask: Task = new Task('', 'high');
     this.tasks.push(createdTask);
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../task.model';
 
 @Component({
   selector: 'app-task-list',
@@ -10,6 +11,33 @@ export class TaskListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  tasks: Task[] = [
+    new Task('Do dishes', 'high'),
+    new Task('Do homework', 'high'),
+    new Task('Beat Tony', 'high'),
+    new Task('Learn To code', 'high'),
+    new Task('Lauch a website', 'high'),
+    new Task('Lose weight', 'high'),
+  ]
+  selectedTask: Task;
+  colourTasks(task: Task): string{
+    if(task.done){
+      return "bg-info"
+    }else{
+      return "bg-danger"
+    }
+  }
+  editTask(task?: Task){
+    if(task){
+      this.selectedTask = task;
+    }else{
+      this.selectedTask = null;
+    }
+  }
+  deleteTask(task: Task){
+    let task_index = this.tasks.indexOf(task);
+    this.tasks.splice(task_index, 1);
   }
 
 }
